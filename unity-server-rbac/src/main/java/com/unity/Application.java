02220f0e73d45b51f1cf82557d6f50370d533fd8
jiajia.geng <gqj19940824@126.com>
2019-09-19 18:55:26 +0800

    update:用户管理去除多余字段

--- unity-server-rbac/src/main/java/com/unity/Application.java
+++ unity-server-rbac/src/main/java/com/unity/Application.java
@@ -5,11 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.context.request.RequestContextListener;
 
 
 /**
@@ -29,11 +27,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public RequestContextListener requestContextListener(){
-        return new RequestContextListener();
     }
 
 }
