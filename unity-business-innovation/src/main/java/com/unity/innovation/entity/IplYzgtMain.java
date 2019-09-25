24abcfd5d230eb8f387980cc84ff6fd27f1dc251
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-09-25 14:59:05 +0800

    代码提交 提交人；耿智强

---
+++ unity-business-innovation/src/main/java/com/unity/innovation/entity/IplYzgtMain.java
@@ -0,0 +1,61 @@
+package com.unity.innovation.entity;
+
+import lombok.*;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import com.unity.innovation.entity.generated.mIplYzgtMain;
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+@Builder(builderMethodName = "newInstance")
+@AllArgsConstructor
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@TableName(value = "ipl_yzgt_main")
+public class IplYzgtMain extends mIplYzgtMain{
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+  
+}
+
---
+++ unity-business-innovation/src/main/java/com/unity/innovation/service/IplYzgtMainServiceImpl.java
@@ -0,0 +1,27 @@
+
+package com.unity.innovation.service;
+
+import com.unity.common.base.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unity.innovation.entity.IplYzgtMain;
+import com.unity.innovation.dao.IplYzgtMainDao;
+
+ /**
+ * 
+ * ClassName: IplYzgtMainService
+ * Function: TODO ADD FUNCTION
+ * Reason: TODO ADD REASON(可选)
+ * date: 2019-09-25 14:51:40
+ * 
+ * @author zhang 
+ * @version  
+ * @since JDK 1.8
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class IplYzgtMainServiceImpl extends BaseServiceImpl<IplYzgtMainDao,IplYzgtMain>{
+
+     
+}
