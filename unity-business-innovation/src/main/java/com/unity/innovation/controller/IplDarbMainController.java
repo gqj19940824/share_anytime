1d5d48ea4266e3de90748a3a3f11d075de5c5b6b
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-10-08 14:01:34 +0800

    将行业类别,需求名目,需求类别对应的ID返回 提交人；耿智强

--- unity-business-innovation/src/main/java/com/unity/innovation/controller/IplDarbMainController.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/controller/IplDarbMainController.java
@@ -272,6 +272,7 @@ public class IplDarbMainController extends BaseWebController {
                     adapterField(m,entity, ids);
                 }
                 ,IplDarbMain::getId,IplDarbMain::getEnterpriseName,IplDarbMain::getProjectName,IplDarbMain::getContent,IplDarbMain::getTotalInvestment,IplDarbMain::getProjectProgress,IplDarbMain::getTotalAmount,IplDarbMain::getBank,IplDarbMain::getBond,IplDarbMain::getSelfRaise,IplDarbMain::getIncreaseTrustType,IplDarbMain::getWhetherIntroduceSocialCapital,IplDarbMain::getConstructionCategory,IplDarbMain::getConstructionStage,IplDarbMain::getConstructionModel,IplDarbMain::getContactPerson,IplDarbMain::getContactWay,IplDarbMain::getAttachmentCode
+                ,IplDarbMain::getIndustryCategory,IplDarbMain::getDemandItem,IplDarbMain::getDemandCategory
         );
     }
     
