355e52f43166bc2766fe6a6ba81d872c3ee31fd7
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-10-16 16:26:54 +0800

    路演接口修改  提交人:耿智强

--- unity-business-innovation/src/main/java/com/unity/innovation/controller/PmInfoDeptController.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/controller/PmInfoDeptController.java
@@ -2,41 +2,29 @@
 package com.unity.innovation.controller;
 
 
-
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.unity.common.constant.DicConstants;
-import com.unity.common.constant.InnovationConstant;
-import com.unity.common.exception.UnityRuntimeException;
-import com.unity.common.pojos.Customer;
-import com.unity.common.ui.PageEntity;
-import com.unity.innovation.entity.generated.IplManageMain;
-import com.unity.innovation.enums.InfoTypeEnum;
-import com.unity.innovation.enums.WorkStatusAuditingStatusEnum;
-import com.unity.innovation.util.InnovationUtil;
-import com.unity.springboot.support.holder.LoginContextHolder;
-import org.apache.commons.lang3.StringUtils;
 import com.unity.common.base.controller.BaseWebController;
+import com.unity.common.constants.ConstString;
 import com.unity.common.pojos.SystemResponse;
 import com.unity.common.ui.PageElementGrid;
-import com.unity.common.ui.SearchElementGrid;
+import com.unity.common.ui.PageEntity;
 import com.unity.common.util.ConvertUtil;
-import com.unity.common.util.DateUtils;
 import com.unity.common.util.JsonUtil;
-import org.assertj.core.util.Lists;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.unity.innovation.entity.InfoDeptSatb;
+import com.unity.innovation.entity.PmInfoDept;
+import com.unity.innovation.enums.InfoTypeEnum;
+import com.unity.innovation.service.InfoDeptSatbServiceImpl;
+import com.unity.innovation.service.PmInfoDeptServiceImpl;
+import com.unity.innovation.util.InnovationUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.unity.common.constants.ConstString;
-import java.util.Map;
-import java.util.List;
-import com.unity.innovation.service.PmInfoDeptServiceImpl;
-import com.unity.innovation.entity.PmInfoDept;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -50,7 +38,8 @@ public class PmInfoDeptController extends BaseWebController {
 
     @Resource
     PmInfoDeptServiceImpl service;
-    
+    @Resource
+    InfoDeptSatbServiceImpl satbService;
 
 
     @PostMapping("/listByPage")
@@ -105,6 +94,44 @@ public class PmInfoDeptController extends BaseWebController {
     public Mono<ResponseEntity<SystemResponse<Object>>>  del(@PathVariable("ids") String ids) {
         service.removeByIds(ConvertUtil.arrString2Long(ids.split(ConstString.SPLIT_COMMA)));
         return success(null);
+    }
+
+    /**
+     * 功能描述 分页列表查询
+     * @param search 查询条件
+     * @return 分页数据
+     * @author gengzhiqiang
+     * @date 2019/9/17 13:36
+     */
+    @PostMapping("/listForSatb")
+    public Mono<ResponseEntity<SystemResponse<Object>>> listForSatb(@RequestBody PageEntity<InfoDeptSatb> search) {
+        IPage<InfoDeptSatb> list = satbService.listForSatb(search);
+        PageElementGrid result = PageElementGrid.<Map<String, Object>>newInstance()
+                .total(list.getTotal())
+                .items(convert2ListForSatb(list.getRecords())).build();
+        return success(result);
+    }
+
+    /**
+     * 功能描述 数据整理
+     * @param list 集合
+     * @return java.util.List<java.util.Map<java.lang.String,java.lang.Object>> 规范数据
+     * @author gengzhiqiang
+     * @date 2019/9/17 13:36
+     */
+    private List<Map<String, Object>> convert2ListForSatb(List<InfoDeptSatb> list) {
+        return JsonUtil.<InfoDeptSatb>ObjectToList(list,
+                (m, entity) -> {
+                },
+                InfoDeptSatb::getId, InfoDeptSatb::getEnterpriseName,
+                InfoDeptSatb::getIndustryCategory, InfoDeptSatb::getIndustryCategoryName,
+                InfoDeptSatb::getEnterpriseScale, InfoDeptSatb::getEnterpriseScaleName,
+                InfoDeptSatb::getEnterpriseNature, InfoDeptSatb::getEnterpriseNatureName,
+                InfoDeptSatb::getInGeneralSituation, InfoDeptSatb::getInDetail,
+                InfoDeptSatb::getAchievementLevel, InfoDeptSatb::getAchievementLevelName,
+                InfoDeptSatb::getIsPublishFirst, InfoDeptSatb::getContactPerson, InfoDeptSatb::getContactWay,
+                InfoDeptSatb::getGmtCreate, InfoDeptSatb::getGmtModified, InfoDeptSatb::getStatus
+        );
     }
 
 }
