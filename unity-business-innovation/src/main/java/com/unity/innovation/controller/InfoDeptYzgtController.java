8f1971605825e5d2999ea110a30c09b5aaae3375
jh <17603638007@163.com>
2019-10-16 14:12:28 +0800

    Resolves CXFB-36 开发入区企业基本信息 add 开发入区企业基本信息

--- unity-business-innovation/src/main/java/com/unity/innovation/controller/InfoDeptYzgtController.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/controller/InfoDeptYzgtController.java
@@ -2,26 +2,27 @@
 package com.unity.innovation.controller;
 
 
-
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.unity.common.enums.YesOrNoEnum;
 import com.unity.common.exception.UnityRuntimeException;
+import com.unity.common.ui.PageEntity;
 import com.unity.common.util.*;
-import com.unity.innovation.entity.SysNotice;
+import com.unity.innovation.util.InnovationUtil;
 import org.apache.commons.lang3.StringUtils;
 import com.unity.common.base.controller.BaseWebController;
 import com.unity.common.pojos.SystemResponse;
 import com.unity.common.ui.PageElementGrid;
-import com.unity.common.ui.SearchElementGrid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.unity.common.constants.ConstString;
 import java.util.Map;
 import java.util.List;
+
+
 import com.unity.innovation.service.InfoDeptYzgtServiceImpl;
 import com.unity.innovation.entity.InfoDeptYzgt;
-
 import javax.annotation.Resource;
 
 
@@ -37,21 +38,26 @@ public class InfoDeptYzgtController extends BaseWebController {
     @Resource
     private InfoDeptYzgtServiceImpl service;
 
-     /**
-     * 获取一页数据
-     * @param search 统一查询条件
-     * @return
-     */
-    @PostMapping("/listByPage")
-    @ResponseBody
-    public Mono<ResponseEntity<SystemResponse<Object>>> listByPage(@RequestBody SearchElementGrid search) {
-    
-        LambdaQueryWrapper<InfoDeptYzgt> ew = wrapper(search);
 
-        IPage p = service.page(search.getPageable(), ew);
+
+
+    /**
+    * 列表查询
+    *
+    * @param pageEntity 分页条件
+    * @return reactor.core.publisher.Mono<org.springframework.http.ResponseEntity<com.unity.common.pojos.SystemResponse<java.lang.Object>>>
+    * @author JH
+    * @date 2019/10/16 10:16
+    */
+    @PostMapping("/listByPage")
+    public Mono<ResponseEntity<SystemResponse<Object>>> listByPage(@RequestBody PageEntity<InfoDeptYzgt> pageEntity) {
+        Page<InfoDeptYzgt> pageable = pageEntity.getPageable();
+        InfoDeptYzgt entity = pageEntity.getEntity();
+        LambdaQueryWrapper<InfoDeptYzgt> ew = wrapper(entity);
+        IPage<InfoDeptYzgt> p = service.page(pageable, ew);
         PageElementGrid result = PageElementGrid.<Map<String,Object>>newInstance()
                 .total(p.getTotal())
-                .items(convert2List(p.getRecords())).build();
+                .items(service.convert2List(p.getRecords())).build();
         return success(result);
 
     }
@@ -72,120 +78,136 @@ public class InfoDeptYzgtController extends BaseWebController {
         return success(null);
     }
 
+    /**
+    * 参数校验
+    *
+    * @param entity 实体
+    * @author JH
+    * @date 2019/10/15 16:46
+    */
     private void validate(InfoDeptYzgt entity) {
         if (entity == null) {
             throw UnityRuntimeException.newInstance().code(SystemResponse.FormalErrorCode.ORIGINAL_DATA_ERR)
                     .message("缺少参数").build();
+        }else {
+            InfoDeptYzgt old = service.getById(entity.getId());
+            if (old == null) {
+                throw UnityRuntimeException.newInstance()
+                        .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
+                        .message("未获取到对象").build();
+            }
+            if (YesOrNoEnum.YES.getType()==old.getStatus()) {
+                throw UnityRuntimeException.newInstance()
+                        .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
+                        .message("已提请发布状态下数据不可编辑").build();
+            }
         }
         //非空校验
         String result = ValidFieldUtil.checkEmptyStr(entity
                 , ValidFieldFactory.emptyReg("行业类别不能为空! ", InfoDeptYzgt::getIndustryCategory)
                 , ValidFieldFactory.emptyReg("企业规模不能为空! ", InfoDeptYzgt::getEnterpriseScale)
-                , ValidFieldFactory.emptyReg("企业性质不能为空! ", InfoDeptYzgt::getEnterpriseNature));
+                , ValidFieldFactory.emptyReg("企业性质不能为空! ", InfoDeptYzgt::getEnterpriseNature)
+                , ValidFieldFactory.emptyReg("企业名称不能为空! ", InfoDeptYzgt::getEnterpriseName)
+                , ValidFieldFactory.emptyReg("企业简介不能为空! ", InfoDeptYzgt::getEnterpriseIntroduction)
+                , ValidFieldFactory.emptyReg("联系人不能为空!   ", InfoDeptYzgt::getContactPerson)
+                , ValidFieldFactory.emptyReg("联系方式不能为空! ", InfoDeptYzgt::getContactWay)
+        );
         if (StringUtils.isNotEmpty(result)) {
             throw UnityRuntimeException.newInstance().code(SystemResponse.FormalErrorCode.ORIGINAL_DATA_ERR)
                     .message(result).build();
         }
-        ValidFieldUtil.checkReg(entity, ValidFieldFactory.lengthReg(1, 20, "标题不能超过50字符!", SysNotice::getTitle));
+        ValidFieldUtil.checkReg(entity
+                ,ValidFieldFactory.lengthReg(1, 50, "企业名称不能超过50字符!", InfoDeptYzgt::getEnterpriseName)
+                ,ValidFieldFactory.lengthReg(1, 20, "企业简介不能超过500字符!", InfoDeptYzgt::getEnterpriseIntroduction)
+                ,ValidFieldFactory.lengthReg(1, 20, "联系人不能超过20字符!", InfoDeptYzgt::getContactPerson)
+                ,ValidFieldFactory.lengthReg(1, 20, "联系方式不能超过50字符!", InfoDeptYzgt::getContactWay)
+        );
+        if(StringUtils.isNotEmpty(entity.getNotes())) {
+            ValidFieldUtil.checkReg(entity, ValidFieldFactory.lengthReg(1, 500, "备注不能超过500字符!", InfoDeptYzgt::getNotes));
+        }
     }
-    
 
 
     /**
-     * 查询条件转换
-     * @param search 统一查询对象
-     * @return
-     */
-    private LambdaQueryWrapper<InfoDeptYzgt> wrapper(SearchElementGrid search){
-        LambdaQueryWrapper<InfoDeptYzgt> ew = null;
-        if(search!=null){
-            if(search.getCond()!=null){
-                search.getCond().findRule(InfoDeptYzgt::getGmtCreate).forEach(r->{
-                   r.setData(DateUtils.parseDate(r.getData()).getTime());
-                });
-                search.getCond().findRule(InfoDeptYzgt::getGmtModified).forEach(r->{
-                   r.setData(DateUtils.parseDate(r.getData()).getTime());
-                });
+    * 详情接口
+    *
+    * @param entity 实体
+    * @return reactor.core.publisher.Mono<org.springframework.http.ResponseEntity<com.unity.common.pojos.SystemResponse<java.lang.Object>>>
+    * @author JH
+    * @date 2019/10/16 9:27
+    */
+    @PostMapping("/detailById")
+    public Mono<ResponseEntity<SystemResponse<Object>>> detailById(@RequestBody InfoDeptYzgt entity) {
+        if(entity.getId() == null) {
+            throw UnityRuntimeException.newInstance().code(SystemResponse.FormalErrorCode.ORIGINAL_DATA_ERR)
+                    .message("id不能为空").build();
+        }
+        return success(service.detailById(entity.getId()));
+    }
+
+
+
+    /**
+    * 查询条件封装
+    *
+    * @param entity 实体
+    * @return com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper<com.unity.innovation.entity.InfoDeptYzgt>
+    * @author JH
+    * @date 2019/10/16 10:28
+    */
+    @SuppressWarnings("unchecked")
+    private LambdaQueryWrapper<InfoDeptYzgt> wrapper(InfoDeptYzgt entity){
+        LambdaQueryWrapper<InfoDeptYzgt> ew = new LambdaQueryWrapper<>();
+        if(entity != null) {
+            //企业名称
+            if (StringUtils.isNotBlank(entity.getEnterpriseName())) {
+                ew.like(InfoDeptYzgt::getEnterpriseName, entity.getEnterpriseName());
             }
-            ew = search.toEntityLambdaWrapper(InfoDeptYzgt.class);
-
+            //行业类型
+            if (entity.getIndustryCategory() != null) {
+                ew.eq(InfoDeptYzgt::getIndustryCategory, entity.getIndustryCategory());
+            }
+            //企业规模
+            if (entity.getEnterpriseScale() != null) {
+                ew.eq(InfoDeptYzgt::getEnterpriseScale, entity.getEnterpriseScale());
+            }
+            //企业性质
+            if (entity.getEnterpriseNature() != null) {
+                ew.eq(InfoDeptYzgt::getEnterpriseNature, entity.getEnterpriseNature());
+            }
+            //创建时间
+            if (StringUtils.isNotBlank(entity.getCreateTime())) {
+                long begin = InnovationUtil.getFirstTimeInMonth(entity.getCreateTime(), true);
+                //gt 大于 lt 小于
+                ew.gt(InfoDeptYzgt::getGmtCreate, begin);
+                long end = InnovationUtil.getFirstTimeInMonth(entity.getCreateTime(), false);
+                ew.lt(InfoDeptYzgt::getGmtCreate, end);
+            }
+            //状态
+            if (entity.getStatus() != null) {
+                ew.eq(InfoDeptYzgt::getStatus, entity.getStatus());
+            }
         }
-        else{
-            ew = new LambdaQueryWrapper<InfoDeptYzgt>();
-        }
-
-        ew.orderBy(true, false,InfoDeptYzgt::getSort);
-        
+        //排序规则      未提请发布在前，已提请发布在后；各自按创建时间倒序
+        ew.orderByDesc(InfoDeptYzgt::getStatus,InfoDeptYzgt::getGmtCreate);
         return ew;
     }
+
     
-    
-    
-     /**
-     * 将实体列表 转换为List Map
-     * @param list 实体列表
-     * @return
-     */
-    private List<Map<String, Object>> convert2List(List<InfoDeptYzgt> list){
-       
-        return JsonUtil.<InfoDeptYzgt>ObjectToList(list,
-                (m, entity) -> {
-                    adapterField(m, entity);
-                }
-                ,InfoDeptYzgt::getId,InfoDeptYzgt::getSort,InfoDeptYzgt::getNotes,InfoDeptYzgt::getEnterpriseName,InfoDeptYzgt::getIndustryCategory,InfoDeptYzgt::getEnterpriseScale,InfoDeptYzgt::getEnterpriseNature,InfoDeptYzgt::getContactPerson,InfoDeptYzgt::getContactWay,InfoDeptYzgt::getEnterpriseIntroduction,InfoDeptYzgt::getAttachmentCode,InfoDeptYzgt::getIdPmInfoDept,InfoDeptYzgt::getStatus
-        );
-    }
-    
-     /**
-     * 将实体 转换为 Map
-     * @param ent 实体
-     * @return
-     */
-    private Map<String, Object> convert2Map(InfoDeptYzgt ent){
-        return JsonUtil.<InfoDeptYzgt>ObjectToMap(ent,
-                (m, entity) -> {
-                    adapterField(m,entity);
-                }
-                ,InfoDeptYzgt::getId,InfoDeptYzgt::getIsDeleted,InfoDeptYzgt::getSort,InfoDeptYzgt::getNotes,InfoDeptYzgt::getEnterpriseName,InfoDeptYzgt::getIndustryCategory,InfoDeptYzgt::getEnterpriseScale,InfoDeptYzgt::getEnterpriseNature,InfoDeptYzgt::getContactPerson,InfoDeptYzgt::getContactWay,InfoDeptYzgt::getEnterpriseIntroduction,InfoDeptYzgt::getAttachmentCode,InfoDeptYzgt::getIdPmInfoDept,InfoDeptYzgt::getStatus
-        );
-    }
-    
+
+
     /**
-     * 字段适配
-     * @param m 适配的结果
-     * @param entity 需要适配的实体
-     */
-    private void adapterField(Map<String, Object> m,InfoDeptYzgt entity){
-        if(!StringUtils.isEmpty(entity.getCreator())) {
-            if(entity.getCreator().indexOf(ConstString.SEPARATOR_POINT)>-1) {
-                m.put("creator", entity.getCreator().split(ConstString.SPLIT_POINT)[1]);
-            }
-            else {
-                m.put("creator", entity.getCreator());
-            }
-        }
-        if(!StringUtils.isEmpty(entity.getEditor())) {
-            if(entity.getEditor().indexOf(ConstString.SEPARATOR_POINT)>-1) {
-                m.put("editor", entity.getEditor().split(ConstString.SPLIT_POINT)[1]);
-            }
-            else {
-                m.put("editor", entity.getEditor());
-            }
-        }
-        
-        m.put("gmtCreate", DateUtils.timeStamp2Date(entity.getGmtCreate()));
-        m.put("gmtModified", DateUtils.timeStamp2Date(entity.getGmtModified()));
-    }
-    
-    /**
-     * 批量删除
-     * @param ids id列表用英文逗号分隔
-     * @return
-     */
-    @DeleteMapping("/del/{ids}")
-    public Mono<ResponseEntity<SystemResponse<Object>>>  del(@PathVariable("ids") String ids) {
-        service.removeByIds(ConvertUtil.arrString2Long(ids.split(ConstString.SPLIT_COMMA)));
-        return success(null);
+    * 批量删除
+    *
+    * @param ids 主键集合
+    * @return reactor.core.publisher.Mono<org.springframework.http.ResponseEntity<com.unity.common.pojos.SystemResponse<java.lang.Object>>>
+    * @author JH
+    * @date 2019/10/16 11:09
+    */
+    @RequestMapping("/removeByIds")
+    public Mono<ResponseEntity<SystemResponse<Object>>> removeByIds(@RequestBody List<Long> ids) {
+        service.deleteByIds(ids);
+        return success("删除成功");
     }
 
 }
