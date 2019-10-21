48586a91db836965ef77903fb50c5b440fb7aa03
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-10-21 09:35:49 +0800

    路演接口修改  提交人:耿智强

--- unity-business-innovation/src/main/java/com/unity/innovation/service/IplManageMainServiceImpl.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/service/IplManageMainServiceImpl.java
@@ -398,6 +398,7 @@ public class IplManageMainServiceImpl extends BaseServiceImpl<IplManageMainDao, 
         }else {
             throw UnityRuntimeException.newInstance().code(SystemResponse.FormalErrorCode.ORIGINAL_DATA_ERR)
                     .message("提交单位错误").build();
+
         }
     }
 
