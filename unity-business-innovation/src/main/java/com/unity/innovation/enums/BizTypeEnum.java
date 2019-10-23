a9bce563542ced753f9675e6fe4970e34ff01712
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-10-23 15:11:11 +0800

    路演接口修改  提交人:耿智强

--- unity-business-innovation/src/main/java/com/unity/innovation/enums/BizTypeEnum.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/enums/BizTypeEnum.java
@@ -8,9 +8,34 @@ import lombok.AllArgsConstructor;
  */
 @AllArgsConstructor
 public enum BizTypeEnum {
-    DARB(10),
-    SATB(20),
-    ESB(30);
+
+    /**
+     * 10城市创新合作 20企业创新发展 30成长目标投资 40高端才智需求 20亲清政商关系
+     */
+    CITY(10),
+    ENTERPRISE(20),
+    GROW(30),
+    INTELLIGENCE(40),
+    POLITICAL(50);
 
     private Integer type;
+
+    public static BizTypeEnum of(Integer type) {
+        if (type.equals(CITY.getType())) {
+            return CITY;
+        } else if (type.equals(ENTERPRISE.getType())) {
+            return ENTERPRISE;
+        } else if (type.equals(GROW.getType())) {
+            return GROW;
+        } else if (type.equals(INTELLIGENCE.getType())) {
+            return INTELLIGENCE;
+        } else {
+            return POLITICAL;
+        }
+    }
+
+    public int getType() {
+        return type;
+    }
+
 }
