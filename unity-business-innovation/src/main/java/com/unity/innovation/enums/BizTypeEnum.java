ee154f80f1d392a2e136a41dd52dc941a14c372e
qinhuan <qinhuanhuan@aspircn.com>
2019-10-23 14:08:38 +0800

    增加类型枚举

---
+++ unity-business-innovation/src/main/java/com/unity/innovation/enums/BizTypeEnum.java
@@ -0,0 +1,16 @@
+package com.unity.innovation.enums;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * <p>
+ * create by qinhuan at 2019/10/23 1:46 下午
+ */
+@AllArgsConstructor
+public enum BizTypeEnum {
+    DARB(10),
+    SATB(20),
+    ESB(30);
+
+    private Integer type;
+}
