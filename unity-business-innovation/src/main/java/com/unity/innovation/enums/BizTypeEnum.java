5403b45116803a0d6b7b28022cc89ec298eb8bf0
jh <17603638007@163.com>
2019-10-23 17:03:11 +0800

    Resolves CXFB-36 开发企业基本信息 update 开发企业基本信息

--- unity-business-innovation/src/main/java/com/unity/innovation/enums/BizTypeEnum.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/enums/BizTypeEnum.java
@@ -10,13 +10,23 @@ import lombok.AllArgsConstructor;
 public enum BizTypeEnum {
 
     /**
-     * 10城市创新合作 20企业创新发展 30成长目标投资 40高端才智需求 20亲清政商关系
+     * 10城市创新合作
+     * 20企业创新发展
+     * 30成长目标投资
+     * 40高端才智需求
+     * 50亲清政商关系
+     * 60入区企业信息发布管理
+     * 70路演企业信息发布管理
+     * 80投资机构信息发布管理
      */
     CITY(10),
     ENTERPRISE(20),
     GROW(30),
     INTELLIGENCE(40),
-    POLITICAL(50);
+    POLITICAL(50),
+    RQDEPTINFO(60),
+    LYDEPTINFO(70),
+    INVESTMENT(80);
 
     private Integer type;
 
@@ -29,9 +39,16 @@ public enum BizTypeEnum {
             return GROW;
         } else if (type.equals(INTELLIGENCE.getType())) {
             return INTELLIGENCE;
-        } else {
+        } else if(type.equals(POLITICAL.getType())) {
             return POLITICAL;
+        } else if(type.equals(RQDEPTINFO.getType())) {
+            return RQDEPTINFO;
+        } else if(type.equals(LYDEPTINFO.getType())) {
+            return LYDEPTINFO;
+        } else {
+            return INVESTMENT;
         }
+
     }
 
     public int getType() {
