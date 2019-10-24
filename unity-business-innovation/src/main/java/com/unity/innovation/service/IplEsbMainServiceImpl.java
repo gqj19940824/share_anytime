8b46f4c3158b431b5eeecc8887935c11ce28525d
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-10-24 19:05:44 +0800

    企服局  提交人:耿智强

--- unity-business-innovation/src/main/java/com/unity/innovation/service/IplEsbMainServiceImpl.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/service/IplEsbMainServiceImpl.java
@@ -1,19 +1,12 @@
 
 package com.unity.innovation.service;
 
-import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.google.common.collect.Lists;
 import com.google.gson.reflect.TypeToken;
 import com.unity.common.base.BaseServiceImpl;
-import com.unity.common.client.RbacClient;
-import com.unity.common.client.vo.DepartmentVO;
 import com.unity.common.constant.DicConstants;
 import com.unity.common.constant.InnovationConstant;
-import com.unity.common.constant.RedisConstants;
-import com.unity.common.enums.YesOrNoEnum;
 import com.unity.common.exception.UnityRuntimeException;
 import com.unity.common.pojos.Customer;
 import com.unity.common.pojos.InventoryMessage;
@@ -23,10 +16,8 @@ import com.unity.common.ui.PageEntity;
 import com.unity.common.util.DateUtils;
 import com.unity.common.util.FileReaderUtil;
 import com.unity.common.util.GsonUtils;
-import com.unity.common.util.JsonUtil;
 import com.unity.common.utils.DicUtils;
 import com.unity.common.utils.ExcelStyleUtil;
-import com.unity.common.utils.HashRedisUtils;
 import com.unity.common.utils.UUIDUtil;
 import com.unity.innovation.constants.ListTypeConstants;
 import com.unity.innovation.dao.IplEsbMainDao;
@@ -34,7 +25,6 @@ import com.unity.innovation.entity.Attachment;
 import com.unity.innovation.entity.IplEsbMain;
 import com.unity.innovation.entity.SysCfg;
 import com.unity.innovation.entity.generated.IplAssist;
-import com.unity.innovation.entity.generated.IplLog;
 import com.unity.innovation.entity.generated.IplManageMain;
 import com.unity.innovation.enums.*;
 import com.unity.innovation.util.InnovationUtil;
@@ -82,15 +72,6 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
 
     @Resource
     private IplAssistServiceImpl iplAssistService;
-
-    @Resource
-    private RbacClient rbacClient;
-
-    @Resource
-    private HashRedisUtils hashRedisUtils;
-
-    @Resource
-    private IplManageMainServiceImpl iplManageMainService;
 
     @Resource
     private SystemConfiguration systemConfiguration;
@@ -194,12 +175,7 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
         if (entity.getId() == null) {
             //判断当前用户是否为操作单位
             if (SourceEnum.SELF.getId().equals((entity.getSource()))) {
-                Customer customer = LoginContextHolder.getRequestAttributes();
-                if (!customer.getTypeRangeList().contains(BizTypeEnum.ENTERPRISE.getType())) {
-                    throw UnityRuntimeException.newInstance()
-                            .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
-                            .message("当前账号的单位不可操作数据").build();
-                }
+                check();
             }
             entity.setAttachmentCode(UUIDUtil.getUUID());
             //来源为当前局
@@ -225,12 +201,7 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
                         .build());
             }
         } else {
-            Customer customer = LoginContextHolder.getRequestAttributes();
-            if (!customer.getTypeRangeList().contains(BizTypeEnum.ENTERPRISE.getType())) {
-                throw UnityRuntimeException.newInstance()
-                        .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
-                        .message("当前账号的单位不可操作数据").build();
-            }
+            check();
             IplEsbMain vo = getById(entity.getId());
             if (IplStatusEnum.DONE.getId().equals(vo.getStatus())) {
                 throw UnityRuntimeException.newInstance()
@@ -250,7 +221,8 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
                         IplStatusEnum.DEALING.getId(),
                         departmentId,
                         0L,
-                        "更新基本信息");
+                        "更新基本信息",
+                        BizTypeEnum.ENTERPRISE.getType());
                 entity.setLatestProcess("更新基本信息");
                 redisSubscribeService.saveSubscribeInfo(entity.getId() + "-0", ListTypeConstants.UPDATE_OVER_TIME,departmentId,BizTypeEnum.ENTERPRISE.getType());
                 //======处理中的数据，主责单位再次编辑基本信息--清单协同处理--增加系统消息=======
@@ -277,12 +249,6 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
      */
     @Transactional(rollbackFor = Exception.class)
     public void removeById(List<Long> ids) {
-        Customer customer = LoginContextHolder.getRequestAttributes();
-        if (!customer.getTypeRangeList().contains(BizTypeEnum.ENTERPRISE.getType())) {
-            throw UnityRuntimeException.newInstance()
-                    .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
-                    .message("当前账号的单位不可操作数据").build();
-        }
         List<IplEsbMain> list = list(new LambdaQueryWrapper<IplEsbMain>().in(IplEsbMain::getId, ids));
         //状态为处理完毕 不可删除
         List<IplEsbMain> doneList = list.stream()
@@ -313,7 +279,8 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
         // 删除主表
         removeByIds(ids);
         // 批量删除主表附带的日志、协同、附件，调用方法必须要有事物
-        iplAssistService.batchDel(ids, InnovationConstant.DEPARTMENT_DARB_ID, codes, BizTypeEnum.ENTERPRISE.getType());
+        Long departmentId = Long.parseLong(dicUtils.getDicValueByCode(DicConstants.DEPART_HAVE_LIST_TYPE, BizTypeEnum.ENTERPRISE.getType()+""));
+        iplAssistService.batchDel(ids, departmentId, codes, BizTypeEnum.ENTERPRISE.getType());
     }
 
     /**
@@ -350,261 +317,6 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
             vo.setAttachmentList(attachmentList);
         }
         return vo;
-    }
-
-    /**
-     * 功能描述  添加协同事项接口
-     * @param entity 实体
-     * @author gengzhiqiang
-     * @date 2019/9/25 18:46
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public void addAssist(IplEsbMain entity) {
-        //主表id  数据集合
-        IplEsbMain vo = getById(entity.getId());
-        if (vo == null) {
-            throw UnityRuntimeException.newInstance()
-                    .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
-                    .message("未获取到对象").build();
-        }
-        List<IplAssist> assistList = entity.getAssistList();
-        StringBuffer sb=new StringBuffer();
-        sb.append("新增协同单位：");
-        assistList.forEach(a -> {
-            if (a.getIdRbacDepartmentAssist() == null) {
-                throw UnityRuntimeException.newInstance()
-                        .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
-                        .message("未获取到协同单位id").build();
-            }
-            //处理状态 正常
-            a.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-            //主表id
-            a.setIdIplMain(vo.getId());
-            //主责单位
-            a.setIdRbacDepartmentDuty(vo.getIdRbacDepartmentDuty());
-            //状态为 正在处理
-            a.setDealStatus(IplStatusEnum.UNDEAL.getId());
-            a.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-            sb.append(hashRedisUtils.getFieldValueByFieldName
-                    (RedisConstants.DEPARTMENT + a.getIdRbacDepartmentAssist(), RedisConstants.NAME).toString());
-            sb.append("、");
-        });
-        sb.deleteCharAt(sb.length() - 1);
-        //保存协同单位
-        iplAssistService.saveBatch(assistList);
-        //保存 主责日志
-        iplLogService.saveLog(entity.getId(), IplStatusEnum.DEALING.getId(),
-                vo.getIdRbacDepartmentDuty(), 0L, sb.toString());
-        vo.setLatestProcess(sb.toString());
-        //更新主表状态
-        if (IplStatusEnum.UNDEAL.getId().equals(vo.getStatus())) {
-            vo.setStatus(IplStatusEnum.DEALING.getId());
-        }
-        vo.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-        updateById(vo);
-    }
-
-
-
-    /**
-     * 功能描述 获取协同单位下拉列表
-     *
-     * @return 单位id及其集合
-     * @author gengzhiqiang
-     * @date 2019/7/26 16:03
-     */
-    public List<Map<String, Object>> getAssistList(IplEsbMain entity) {
-        //主表id  数据集合
-        IplEsbMain vo = getById(entity.getId());
-        if (vo == null) {
-            throw UnityRuntimeException.newInstance()
-                    .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
-                    .message("未获取到对象").build();
-        }
-        List<DepartmentVO> departmentList = rbacClient.getAllDepartment();
-        List<IplAssist> assistList = iplAssistService.list(new LambdaQueryWrapper<IplAssist>()
-                .eq(IplAssist::getIdIplMain, vo.getId())
-                .eq(IplAssist::getIdRbacDepartmentDuty, vo.getIdRbacDepartmentDuty()));
-        List<Long> ids = assistList.stream().map(IplAssist::getIdRbacDepartmentAssist).collect(Collectors.toList());
-        departmentList = departmentList.stream().filter(d -> !ids.contains(d.getId())).collect(Collectors.toList());
-        return JsonUtil.ObjectToList(departmentList, new String[]{"id", "name"}, null);
-    }
-
-
-
-    /**
-     * 功能描述 主责单位处理协同单位接口
-     * @param entity  协同单位实体
-     * @author gengzhiqiang
-     * @date 2019/9/25 18:46
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public void dealAssist(IplAssist entity) {
-        //主表id
-        IplEsbMain vo = getById(entity.getIdIplMain());
-        if (vo == null) {
-            throw UnityRuntimeException.newInstance()
-                    .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
-                    .message("未获取到对象").build();
-        }
-        //获取协同单位名称
-        String assistDeptName = hashRedisUtils.getFieldValueByFieldName
-                (RedisConstants.DEPARTMENT + entity.getIdRbacDepartmentAssist(), RedisConstants.NAME);
-        //处理中
-        if (IplStatusEnum.DEALING.getId().equals(entity.getDealStatus())) {
-            //保存 协同日志
-            iplLogService.saveLog(vo.getId(),IplStatusEnum.DEALING.getId(),vo.getIdRbacDepartmentDuty(), entity.getIdRbacDepartmentAssist(),
-                    "主责单位开启协同邀请");
-            //保存 主责日志
-            iplLogService.saveLog(vo.getId(),IplStatusEnum.DEALING.getId(),vo.getIdRbacDepartmentDuty(), 0L,
-                    "开启"+assistDeptName+"协同邀请");
-            //更新主表状态
-            if (IplStatusEnum.UNDEAL.getId().equals(vo.getStatus())) {
-                vo.setStatus(IplStatusEnum.DEALING.getId());
-            }
-            vo.setLatestProcess("开启"+assistDeptName+"协同邀请");
-            vo.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-            updateById(vo);
-            //更新协同单位状态
-            entity.setDealStatus(IplStatusEnum.DEALING.getId());
-            entity.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-            iplAssistService.update(entity, new LambdaUpdateWrapper<IplAssist>()
-                    .eq(IplAssist::getIdRbacDepartmentDuty, InnovationConstant.DEPARTMENT_ESB_ID)
-                    .eq(IplAssist::getIdIplMain, vo.getId())
-                    .eq(IplAssist::getIdRbacDepartmentAssist, entity.getIdRbacDepartmentAssist()));
-        } else if (IplStatusEnum.DONE.getId().equals(entity.getDealStatus())) {
-            //保存 协同日志
-            iplLogService.saveLog(vo.getId(),IplStatusEnum.DONE.getId(),vo.getIdRbacDepartmentDuty(), entity.getIdRbacDepartmentAssist(),
-                    "主责单位关闭协同邀请");
-            //保存 主责日志
-            iplLogService.saveLog(vo.getId(),IplStatusEnum.DONE.getId(),vo.getIdRbacDepartmentDuty(), 0L,
-                    "关闭" + assistDeptName + "协同邀请");
-            //主表状态
-            vo.setStatus(IplStatusEnum.DONE.getId());
-            vo.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-            vo.setLatestProcess("关闭"+assistDeptName+"协同邀请");
-            updateById(vo);
-            //日志表状态
-            entity.setDealStatus(IplStatusEnum.DONE.getId());
-            entity.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-            iplAssistService.update(entity, new LambdaUpdateWrapper<IplAssist>()
-                    .eq(IplAssist::getIdRbacDepartmentDuty, InnovationConstant.DEPARTMENT_ESB_ID)
-                    .eq(IplAssist::getIdIplMain, vo.getId())
-                    .eq(IplAssist::getIdRbacDepartmentAssist, entity.getIdRbacDepartmentAssist()));
-        }
-
-    }
-
-    /**
-     * 功能描述 实时更新处理接口
-     * @param entity  实体
-     * @author gengzhiqiang
-     * @date 2019/9/25 18:46
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public void updateStatus(IplAssist entity) {
-        //主表id  数据集合
-        IplEsbMain vo = getById(entity.getIdIplMain());
-        if (vo == null) {
-            throw UnityRuntimeException.newInstance()
-                    .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
-                    .message("未获取到对象").build();
-        }
-        //判断是主责和协同处理 0指的是主责单位
-        if (YesOrNoEnum.NO.getType() == entity.getIdRbacDepartmentAssist()) {
-            //处理中
-            if (IplStatusEnum.DEALING.getId().equals(entity.getDealStatus())) {
-                //保存 主责日志
-                iplLogService.saveLog(entity.getIdIplMain(), IplStatusEnum.DEALING.getId(),
-                        vo.getIdRbacDepartmentDuty(), 0L, entity.getDealMessage());
-                //更新主表状态
-                if (IplStatusEnum.UNDEAL.getId().equals(vo.getStatus())) {
-                    vo.setStatus(IplStatusEnum.DEALING.getId());
-                }
-                vo.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-                vo.setLatestProcess(entity.getDealMessage());
-                updateById(vo);
-            } else if (IplStatusEnum.DONE.getId().equals(entity.getDealStatus())) {
-                //处理 未完成的协同单位数据
-                List<IplAssist> dealData = iplAssistService.list(new LambdaQueryWrapper<IplAssist>()
-                        .eq(IplAssist::getIdIplMain, entity.getIdIplMain())
-                        .eq(IplAssist::getIdRbacDepartmentDuty, vo.getIdRbacDepartmentDuty())
-                        .ne(IplAssist::getIdRbacDepartmentAssist, 0L)
-                        .ne(IplAssist::getDealStatus, IplStatusEnum.DONE.getId()));
-                if (CollectionUtils.isNotEmpty(dealData)) {
-                    List<IplLog> logs = Lists.newArrayList();
-                    StringBuffer sb = new StringBuffer();
-                    sb.append(entity.getDealMessage());
-                    sb.append(System.getProperty(InnovationConstant.LINE_SEPARATOR));
-                    sb.append("关闭");
-                    dealData.forEach(d -> {
-                        //遍历单位名称，拼接日志记录
-                        sb.append(hashRedisUtils.getFieldValueByFieldName
-                                (RedisConstants.DEPARTMENT + d.getIdRbacDepartmentAssist(), RedisConstants.NAME).toString());
-                        sb.append("、");
-                        IplLog log = IplLog.newInstance()
-                                .dealStatus(IplStatusEnum.DONE.getId())
-                                .idIplMain(vo.getId())
-                                .idRbacDepartmentDuty(vo.getIdRbacDepartmentDuty())
-                                .idRbacDepartmentAssist(d.getIdRbacDepartmentAssist())
-                                .processInfo("主责单位关闭协同邀请")
-                                .build();
-                        logs.add(log);
-                    });
-                    //保存协同日志
-                    iplLogService.saveBatch(logs);
-                    //修改协同单位状态
-                    IplAssist assist=IplAssist.newInstance()
-                            .dealStatus(IplStatusEnum.DONE.getId())
-                            .processStatus(ProcessStatusEnum.NORMAL.getId())
-                            .build();
-                    iplAssistService.update(assist, new LambdaUpdateWrapper<IplAssist>()
-                            .eq(IplAssist::getIdRbacDepartmentDuty, InnovationConstant.DEPARTMENT_ESB_ID)
-                            .eq(IplAssist::getIdIplMain, vo.getId()));
-                    sb.deleteCharAt(sb.length() - 1);
-                    sb.append("协同邀请");
-                    entity.setDealMessage(sb.toString());
-                }
-                //保存 主责日志
-                iplLogService.saveLog(entity.getIdIplMain(), IplStatusEnum.DONE.getId(),
-                        vo.getIdRbacDepartmentDuty(), 0L, entity.getDealMessage());
-                vo.setLatestProcess(entity.getDealMessage());
-                //更新主表状态
-                vo.setStatus(IplStatusEnum.DONE.getId());
-                vo.setProcessStatus(ProcessStatusEnum.NORMAL.getId());
-                updateById(vo);
-            }
-        }
-        //协同处理单位id不为 0 else {}
-
-    }
-
-    /**
-     * 功能描述 详情接口
-     * @param entity 实体
-     * @return com.unity.innovation.entity.generated.IplManageMain 对象
-     * @author gengzhiqiang
-     * @date 2019/10/9 19:50
-     */
-    public IplManageMain detailByIdForPkg(IplManageMain entity) {
-        entity = iplManageMainService.getById(entity.getId());
-        if (entity == null) {
-            throw UnityRuntimeException.newInstance()
-                    .code(SystemResponse.FormalErrorCode.LACK_REQUIRED_PARAM)
-                    .message("未获取到对象").build();
-        }
-        //快照集合
-        List<IplEsbMain> list = JSON.parseArray(entity.getSnapshot(), IplEsbMain.class);
-        entity.setSnapshot("");
-       // entity.setIplEsbMainList(list);
-        //附件
-        List<Attachment> attachmentList = attachmentService.list(new LambdaQueryWrapper<Attachment>().eq(Attachment::getAttachmentCode, entity.getAttachmentCode()));
-        if (CollectionUtils.isNotEmpty(attachmentList)) {
-            entity.setAttachments(attachmentList);
-        }
-        //日志集合 日志节点集合
-        entity = iplManageMainService.setLogs(entity);
-        return entity;
     }
 
 
@@ -745,5 +457,12 @@ public class IplEsbMainServiceImpl extends BaseServiceImpl<IplEsbMainDao, IplEsb
         RegionUtil.setBorderTop(1, range, sheet);
     }
 
-
+    public void check(){
+        Customer customer = LoginContextHolder.getRequestAttributes();
+        if (!customer.getTypeRangeList().contains(BizTypeEnum.ENTERPRISE.getType())) {
+            throw UnityRuntimeException.newInstance()
+                    .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
+                    .message("当前账号的单位不可操作数据").build();
+        }
+    }
 }
