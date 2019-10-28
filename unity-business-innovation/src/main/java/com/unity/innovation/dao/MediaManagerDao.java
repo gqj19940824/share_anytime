49a1237b08a32c910d2ddc3e5fb7e828c2ce8337
jh <17603638007@163.com>
2019-10-28 14:46:13 +0800

    Resolves CXFB-51 开发媒体管理 update 开发媒体管理

---
+++ unity-business-innovation/src/main/java/com/unity/innovation/dao/MediaManagerDao.java
@@ -0,0 +1,16 @@
+
+package com.unity.innovation.dao;
+
+
+import com.unity.common.base.BaseDao;
+import com.unity.innovation.entity.MediaManager;
+
+/**
+ * 媒体管理表
+ * @author zhang
+ * 生成时间 2019-10-28 13:41:56
+ */
+public interface MediaManagerDao  extends BaseDao<MediaManager>{
+	
+}
+
