486ee69ac22829f64a8e2d842434a491f9c2074e
zhiqiang.geng <gengzhiqiang@jingcaiwang.cn>
2019-11-18 11:28:59 +0800

    提示语修改  提交人:耿智强

--- unity-business-innovation/src/main/java/com/unity/innovation/service/DailyWorkPackageServiceImpl.java
+++ unity-business-innovation/src/main/java/com/unity/innovation/service/DailyWorkPackageServiceImpl.java
@@ -59,7 +59,7 @@ public class DailyWorkPackageServiceImpl extends BaseServiceImpl<DailyWorkPackag
             if (works.size() > exist.size()) {
                 throw UnityRuntimeException.newInstance()
                         .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
-                        .message("添加数据存在已删除的数据").build();
+                        .message("所添加数据中存在已删除或已提请数据，请重新添加！").build();
             }
             List<DailyWorkPackage> list = Lists.newArrayList();
             works.forEach(i -> {
@@ -87,7 +87,7 @@ public class DailyWorkPackageServiceImpl extends BaseServiceImpl<DailyWorkPackag
             if (add.size() > existAdd.size()) {
                 throw UnityRuntimeException.newInstance()
                         .code(SystemResponse.FormalErrorCode.ILLEGAL_OPERATION)
-                        .message("添加数据存在已删除的数据").build();
+                        .message("所添加数据中存在已删除或已提请数据，请重新添加！").build();
             }
             List<DailyWorkPackage> list = Lists.newArrayList();
             add.forEach(i -> {
